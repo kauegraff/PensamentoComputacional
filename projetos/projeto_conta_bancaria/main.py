0aca9f555fb39e00c769210e3ce3f66549967530
Kauê Graff <kauegraff6@gmail.com>
2025-05-06 21:53:38 -0300

    changed file ContaBancaria.py
    addinting verifications in class "ContaBancaria"

--- projetos/projeto_conta_bancaria/main.py
+++ projetos/projeto_conta_bancaria/main.py
@@ -1,48 +1,13 @@
 from models.ContaBancaria import ContaBancaria
 
-conta1 = ContaBancaria('Kauê', 1349.40, 1200)
-conta2 = ContaBancaria('Eduarda', 0, 4000)
+conta1 = ContaBancaria('Kauê', 1300, 1200, [])
+conta2 = ContaBancaria('Jose', 2000, 1500, [])
 
-print("Conta Bancária")
-
-# Tela Login -> Login ou Cadastro
-
-opcao = input("""\n Qual operação você deseja realizar: \n
-    1 - Depositar
-    2 - Sacar 
-    3 - Transferir
-""")
-
-if opcao == "1":
-    valor = float(input("Digite o valor que você deseja depositar: "))
-    conta1.depositar(valor)
-elif opcao == "2":
-    valor = float(input("Digite o valor que você deseja sacar: "))
-    conta1.sacar(valor)
-elif opcao == "3":
-    valor = float(input("Digite o valor que você deseja transferir: "))
-    conta1.transferir(valor, conta2)
-else:
-    print("Opção digitada inválida")
-
-
-
-"""conta1.exibir_saldo()
-conta1.depositar(10000)
-conta1.exibir_saldo()
-conta1.sacar(2000)
-conta1.exibir_saldo()
-
+conta1.depositar(150)
+conta1.exibir_historico()
+conta1.sacar(100)
+conta1.exibir_historico()
 conta1.transferir(1000, conta2)
-
-conta1.exibir_saldo()
 conta1.exibir_historico()
-conta2.exibir_historico()
-conta2.exibir_saldo()
-conta2.exibir_saldo()"""
-
-conta1.exibir_saldo()
-conta1.exibir_historico()
-conta2.exibir_saldo()
 conta2.exibir_historico()
 
--- projetos/projeto_conta_bancaria/models/ContaBancaria.py
+++ projetos/projeto_conta_bancaria/models/ContaBancaria.py
@@ -1,5 +1,17 @@
+import time
+
 class ContaBancaria:
-    def __init__(self, titular, saldo, limite):
+    '''
+    Classe que implementa métodos para manipular uma conta bancária.
+    Atributos: titular (str), saldo (float), limite (float) e históricos (lista de dicionários) 
+
+    OBS: Operações no histórico: 0 - Sacar, 1 - Depositar, 2 - Transferir
+    '''
+
+    def __init__(self, titular, saldo, limite, historico):
+        '''
+        Construtor da classe ContaBancaria
+        '''
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
@@ -7,32 +19,52 @@ class ContaBancaria:
         self.id_historico = 0
     
     def depositar(self, valor):
+        '''
+        Objetivo: Método que realiza o depósito na conta bancária.
+
+        Entrada: valor (float)
+
+        Return: True, se a operação foi realizada com sucesso. False, se a operação não foi realizada. 
+
+        '''
         if valor > 0:
             self.saldo += valor
-            self.id_historico += 1
-            self.salvar_historico("Deposito", valor, " ")
-            """self.historico.append({
+            self.historico.append({
+                "operacao": 1, 
+                "remetente": self.titular, 
+                "destinatario": None, 
                 "valor": valor, 
-                "titular_conta": self.titular, 
-                "tipo_transacao": "Deposito", 
-                "id_transacao": self.id_historico, 
-                "natureza": " "
-            })"""
+                "saldo": self.saldo,
+                "dataetempo": int(time.time())
+            }) 
+
+            return True
         else:
             print(f"O valor {valor} é inválido!")
+            return False
         # Adicionar na lista de histórico
     
     def sacar(self, valor):
+        '''
+        Objetivo: Método que realiza o saque na conta bancária.
+
+        Entrada: valor (float)
+
+        Return: True, se a operação foi realizada com sucesso. False, se a operação não foi realizada. 
+
+        '''
+
         if self.saldo >= valor:
             self.saldo -= valor
-            self.id_historico += 1
             self.historico.append({
+                "operacao": 0, 
+                "remetente": self.titular, 
+                "destinatario": None, 
                 "valor": valor, 
-                "titular_conta": self.titular, 
-                "tipo_transacao": "Sacar", 
-                "id_transacao": self.id_historico, 
-                "natureza": "Saldo"
+                "saldo": self.saldo,
+                "dataetempo": int(time.time())
             }) 
+            return True
         else:
             a = input(f"Deseja utilizar o limite? (R$ {self.limite}) [S para sim]")
             if a == "S":
@@ -40,78 +72,59 @@ class ContaBancaria:
                     self.saldo -= valor
                     print("Saque Realizado!")
 
-                    self.id_historico += 1
                     self.historico.append({
-                    "valor": valor, 
-                    "titular_conta": self.titular, 
-                    "tipo_transacao": "Saque", 
-                    "id_transacao": self.id_historico, 
-                    "natureza": "Limite"
-                    })    
+                        "operacao": 0, 
+                        "remetente": self.titular, 
+                        "destinatario": None, 
+                        "valor": valor, 
+                        "saldo": self.saldo,
+                        "dataetempo": int(time.time())
+                    }) 
+                    return True 
                 else:
                     print("Saldo e limite insuficientes para realizar a operação!")
             else:
                 print("Operação com limite cancelada!")
+        return False
         # Adicionar na lista de histórico
     
     def transferir(self, valor, conta_destino):
         if self.saldo > valor:
             self.saldo -= valor
-            self.id_historico += 1
             self.historico.append({
+                "operacao": 2, 
+                "remetente": self.titular, 
+                "destinatario": conta_destino.titular, 
                 "valor": valor, 
-                "titular_conta": self.titular, 
-                "tipo_transacao": "Transferência Enviada", 
-                "id_transacao": self.id_historico, 
-                "natureza": "Saldo"
+                "saldo": self.saldo,
+                "dataetempo": int(time.time())
+            }) 
+            conta_destino.saldo += valor
+            conta_destino.historico.append({
+                "operacao": 2, 
+                "remetente": self.titular, 
+                "destinatario": conta_destino.titular, 
+                "valor": valor, 
+                "saldo": conta_destino.saldo,
+                "dataetempo": int(time.time())
             })
 
-            conta_destino.saldo += valor
-            conta_destino.id_historico += 1
-            conta_destino.historico.append({
-                "valor": valor, 
-                "titular_conta": conta_destino.titular, 
-                "tipo_transacao": "Transferência Recebida", 
-                "id_transacao": conta_destino.id_historico, 
-                "natureza": "Saldo"
-            })
         else:
             print("Saldo insuficiente para realizar a operação!")
 
     def exibir_historico(self):
-        print(self.historico)
+        print(f"Histórico de transações de {self.titular}:")
+        for transacao in self.historico:
+            dt = time.localtime(transacao["dataetempo"])
+
+            print("Operação:", transacao["operacao"], 
+                  "| Remetente:", transacao["remetente"], 
+                  "| Destinatário:", transacao["destinatario"], 
+                  "| Valor:", transacao["valor"], 
+                  "| Saldo:", transacao["saldo"], 
+                  "| Data e tempo:", str(dt.tm_hour) + ":" + str(dt.tm_min) + ":" + str(dt.tm_sec) + " " + 
+                  str(dt.tm_mday) + "/" + str(dt.tm_mon) + "/" + str(dt.tm_year))
 
     def exibir_saldo(self):
         print(f"Saldo: {self.saldo} Limite: {self.limite}")
     
-    def salvar_historico(conta, tipo_transacao, valor, natureza):
-        if tipo_transacao == "Deposito":
-            conta.historico.append({
-                "valor": valor, 
-                "titular_conta": conta.titular, 
-                "tipo_transacao": "Deposito", 
-                "id_transacao": conta.id_historico, 
-                "natureza": " "
-        })
-        elif tipo_transacao == "Sacar":
-            if natureza == "Saldo":
-                conta.historico.append({
-                    "valor": valor, 
-                    "titular_conta": conta.titular, 
-                    "tipo_transacao": "Sacar", 
-                    "id_transacao": conta.id_historico, 
-                    "natureza": "Saldo"
-                })
-            elif natureza == "Limite":
-                conta.historico.append({
-                    "valor": valor, 
-                    "titular_conta": conta.titular, 
-                    "tipo_transacao": "Sacar", 
-                    "id_transacao": conta.id_historico, 
-                    "natureza": "Limite"
-                })
-            else: 
-                print("Natureza inválida!")
-        elif tipo_transacao == "Transferencia":
-            pass
-
